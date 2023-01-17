8a2ad7a17557fd1fc9c2594fde2558cccf2e42dd
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-01-17 16:58:02 +0800

    add socket server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 const app = express();
 
 app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+});
 
 server.listen(4000, () => {
   console.log('Server is running on port 4000');
