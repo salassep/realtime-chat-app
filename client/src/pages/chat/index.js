8a8637c3fbc5970caabf08929a291e6871b360d6
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-01-17 23:37:14 +0800

    add send message component

--- client/src/pages/chat/index.js
+++ client/src/pages/chat/index.js
@@ -1,11 +1,13 @@
 import styles from './styles.module.css';
 import Messages from './messages';
+import SendMessage from './send-messages';
 
-const Chat = ({ socket }) => {
+const Chat = ({ username, room, socket }) => {
   return (
     <div className={styles.chatContainer}>
       <div>
         <Messages socket={socket} />
+        <SendMessage socket={socket} username={username} room={room} />
       </div>
     </div>
   );
