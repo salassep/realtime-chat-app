1f7900075ee527fbd6f9ba8bd3823ca372f2fff4
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-01-17 20:58:54 +0800

    set socket room

--- server/index.js
+++ server/index.js
@@ -16,8 +16,40 @@ const io = new Server(server, {
   },
 });
 
+const CHAT_BOT = 'ChatBot';
+let chatRoom = '';
+let allUsers = [];
+
 io.on('connection', (socket) => {
   console.log(`User connected ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    const { username, room } = data;
+    socket.join(room);
+
+    // eslint-disable-next-line no-underscore-dangle
+    const __createdTime__ = Date.now();
+
+    socket.to(room).emit('receive_message', {
+      message: `${username} has joined the chat room`,
+      username: CHAT_BOT,
+      __createdTime__,
+    });
+
+    socket.emit('receive_message', {
+      message: `Welcome ${username}`,
+      username: CHAT_BOT,
+      __createdTime__,
+    });
+
+    chatRoom = room;
+    allUsers.push({ id: socket.id, username, room });
+
+    const chatRoomUsers = allUsers.filter((user) => user.room === room);
+
+    socket.to(room).emit('chatroom_users', chatRoomUsers);
+    socket.emit('chatroom_users', chatRoomUsers);
+  });
 });
 
 server.listen(4000, () => {
